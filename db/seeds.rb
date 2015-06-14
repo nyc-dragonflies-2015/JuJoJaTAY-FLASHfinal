593ede360bc70ba9e94be8ea46dc8d975b9ef74e
johnlyden <john.lyden6@gmail.com>
2015-06-14 15:54:50 -0400

    minor css changes and increased size of navbar text

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@ File.open('db/fixtures/harry.txt').each_slice(2) {|question, answer| Card.create
 turtle_deck = Deck.create(name: "Teenage Mutant Ninja Turtles Trivia", description: "Test your Ninja Turtle knowledge you dork")
 
 File.open('db/fixtures/turtles.txt').each_slice(2) {|question, answer| Card.create(question: question.chomp, answer: answer.chomp).update_attributes(deck_id: turtle_deck.id)}
+
+tv = Deck.create(name: "Dope TV Shows", description: "Test your knoweldge of great tv shows from the past")
+
+File.open('db/fixtures/classic_tv.txt').each_slice(2) {|question, answer| Card.create(question: question.chomp, answer: answer.chomp).update_attributes(deck_id: tv.id)}
